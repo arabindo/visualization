1584ccc0cbd744f0c8134551b23ed293d39425e3
arabindo <arobinda7@gmail.com>
2020-04-20 18:59:17 +0530

    A dictionary poly{} added to make the programme dynamic. Hermite function is implemented.

--- lho.py
+++ lho.py
@@ -6,8 +6,30 @@ from bokeh.models import ColumnDataSource, Slider, TextInput
 from bokeh.plotting import figure, output_file, show
 
 #Function to get values of Hermite Polynomial
+
+poly = {} #this dictionary is used to remember the hermite polynomials already calculated
+
 def hermite(n):
-    pass
+    
+    var = np.linspace(-20, 20, 10000)
+    
+    if (n in poly):
+        return poly[n]
+        
+    elif  n==0:
+        val0 = 1
+        return val0
+        
+    elif n==1:
+        val_1 = 2 * var 
+        val=val_1
+        poly[1]= val
+        return val
+        
+    else:
+        value = ((2*var*hermite(n-1)) - (2*(n-1)*hermite(n-2)))
+        poly[n]=value
+        return value
     
     
 
