54db76d399164ecf8f3f2097ff1839382eceb701
arabindo <arobinda7@gmail.com>
2020-04-20 18:47:57 +0530

    lho.py file is added function for hermite polynomial is decleared.

---
+++ lho.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.plotting import figure, output_file, show
+
+#Function to get values of Hermite Polynomial
+def hermite(n):
+    pass
+    
+    
+
+#set up data
+
+
+
+
+#set up plot
+
+
+
+#set up widgets
+
+
+
+#set up callback
+
+
+
+#setup layout
